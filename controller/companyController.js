a242787e0b680e531e2ba8bfba71da626cd440b2
jogey.kristen <jogey.kristen@centelon.com>
2023-05-02 18:44:29 +0530

    validation_added_to_company

--- controller/companyController.js
+++ controller/companyController.js
@@ -18,8 +18,8 @@ const upload = multer();
 
 module.exports.signup = async(req,res)=>{
     
-    const currentsDate = req.currentDate;
-    console.log('date =',req.currentDate);
+    // const currentsDate = req.currentDate;
+    // console.log('date =',req.currentDate);
 
     upload.none()(req, res, async (err) => {
         if (err) {
@@ -27,39 +27,65 @@ module.exports.signup = async(req,res)=>{
     }
 
         saltRounds = 10;
+        const email = req.body.company_email;
+        const number = req.body.company_number;
+        var emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+        regex = /^\d{10}$/;
         
-        const company = await Company.findOne({company_email:req.body.company_email})
-        if(company)return res.status(400).send("Company already registered")
+        if(emailFormat.test(email)){
+            if(regex.test(number)){
+                const company = await Company.findOne({company_email:req.body.company_email})
+                if(company)return res.status(400).send("Company already registered")
 
-        const pass = req.body.company_password;
-        
-        const hashedPassword =await bcrypt.hash(pass,saltRounds)
+                const pass = req.body.company_password;
+                
+                const hashedPassword =await bcrypt.hash(pass,saltRounds)
 
-        const comp = new Company({
-            company_name:req.body.company_name,
-            company_number:req.body.company_number,
-            company_email:req.body.company_email,
-            company_password:hashedPassword
-        
-        });
+                
 
-        const result = await comp.save();
+
+                const comp = new Company({
+                    company_name:req.body.company_name,
+                    company_number:req.body.company_number,
+                    company_email:req.body.company_email,
+                    company_password:hashedPassword
+                
+                });
+
+                const result = await comp.save();
+                
+                return res.status(200).send("Company created successfully" )
+            
+            }
+            else{
+                return res.status(400).send("Enter a 10 digit phone number")
+            }
+        }
+        else{
+            return res.status(400).send("Enter a valid email address")
+        }
+    })
         
-        return res.status(200).send("Company created successfully" )
-    });
 }
 
 module.exports.login = async(req,res)=>{
     const email = req.body.company_email;
     const password = req.body.company_password;
+    var emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
-    const User =await Company.findOne({
-        company_email:email
-    })
-    if(!User){return res.status(400).send("No Company found")}
-    const check =await bcrypt.compare(password,User.company_password)
-
-    if(!check) return res.status(400).send("Invalid password")
-    const token =  jwt.sign({ company_email: User.company_email,  _id: User._id }, process.env.JWT_SECERET_KEY, { expiresIn: '1h' });
-    return res.status(200).send({data:token})
+    if(emailFormat.test(email)){
+        const User =await Company.findOne({
+            company_email:email
+        })
+        if(!User){return res.status(400).send("No Company found")}
+        const check =await bcrypt.compare(password,User.company_password)
+    
+        if(!check) return res.status(400).send("Invalid password")
+        const token =  jwt.sign({ company_email: User.company_email,  _id: User._id }, process.env.JWT_SECERET_KEY, { expiresIn: '1h' });
+        return res.status(200).send({data:token})
+    }
+    else{
+        return res.status(400).send("Enter a valid email address")
+    }
+    
 }
